c9d13a3090640603e7ab8c27857b61d43c7125f2
ReynaldAvrilliant <reynald.avrilliant96@gmail.com>
2020-02-13 10:49:54 +0700

    komit ke 2

---
+++ test/0.py
@@ -0,0 +1,14 @@
+print("x")
+print("x")
+print("x")
+print("x")
+print("x")
+print("x")
+print("x")
+print("x")
+# bikin commit
+# 1. add files to stagging area.
+print("x")
+print("x")
+print("x")
+print("x")
